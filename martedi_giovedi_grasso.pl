ebb68dff7a48626c049b02d611187ca623928f2f
ImBelo <gianmarco.beligni@gmail.com>
2025-04-09 23:54:46 +0200

    aggiunto eccezzioni

--- martedi_giovedi_grasso.pl
+++ martedi_giovedi_grasso.pl
@@ -1,21 +1,35 @@
+mese(febbraio).
+mese(marzo).
+mese(aprile).
+precedente(febbraio,marzo).
+precedente(marzo,aprile).
+precedente(aprile,maggio).
+successivo(Mese1,Mese2):-
+  precedente(Mese2,Mese1).
+
 pasqua(Giorno, Mese, Anno) :-
     A is Anno mod 19,
     B is Anno mod 4,
     C is Anno mod 7,
-    M_greg is 24,
-    N_greg is 5,
-    D is (19 * A + M_greg) mod 30,
-    E is (2*B+4*C + 6 * D + N_greg) mod 7,
+    D is (19*A + 24) mod 30,
+    E is (2*B + 4*C + 6*D + 5) mod 7,
     Z is D + E,
+
+    % Determine base date
     (Z < 10 -> 
-      Giorno is Z + 22,
-      Mese = 3
-    ;
-      Giorno is Z - 9,
-      Mese = 4
+        BaseG is 22 + Z, BaseM = marzo
+    ;   
+        BaseG is Z - 9, BaseM = aprile
+    ),
+
+    % Apply exceptions
+    (BaseG =:= 26, BaseM = aprile -> 
+        Giorno = 19, Mese = aprile
+    ; BaseG =:= 25, BaseM = aprile, D =:= 28, E =:= 6, A > 10 -> 
+        Giorno = 18, Mese = aprile
+    ; 
+        Giorno = BaseG, Mese = BaseM
     ).
-
-
 controlla_bisestile(Anno) :-
     (Anno mod 400 =:= 0; 
      (Anno mod 100 =\= 0, Anno mod 4 =:= 0)).  
@@ -26,32 +40,24 @@ crea_data(Giorno, Mese, Anno) :-
     giorni_del_mese(GiornoMax, Mese, Anno),
     Giorno =< GiornoMax.
 
-giorni_del_mese(28, 2, Anno) :- \+ controlla_bisestile(Anno).
-giorni_del_mese(29, 2, Anno) :- controlla_bisestile(Anno).
-giorni_del_mese(31, Mese, _) :- member(Mese, [1,3,5,7,8,10,12]).
-giorni_del_mese(30, Mese, _) :- member(Mese, [4,6,9,11]).
+giorni_del_mese(28, febbraio, Anno) :- \+ controlla_bisestile(Anno).
+giorni_del_mese(29, febbraio, Anno) :- controlla_bisestile(Anno).
+giorni_del_mese(31, Mese, _) :-Mese = marzo.
+giorni_del_mese(30, Mese, _) :-Mese = aprile.
 sottrai_giorni(Sottraendo, data(Giorno, Mese, Anno), DataArrivo) :-
     GiornoSufficiente is Giorno - Sottraendo,
     (GiornoSufficiente > 0 -> 
         DataArrivo = data(GiornoSufficiente, Mese, Anno)
     ;
-        (Mese =:= 1 ->
-            NuovoMese is 12,
-            NuovoAnno is Anno - 1
-        ;
-            NuovoMese is Mese - 1,
-            NuovoAnno is Anno
-        ),
-        giorni_del_mese(MaxGiorni, NuovoMese, NuovoAnno),
-        NuovoGiorno is Giorno - Sottraendo + MaxGiorni,
-        sottrai_giorni(0, data(NuovoGiorno, NuovoMese, NuovoAnno), DataArrivo)
+       precedente(NuovoMese,Mese),
+       giorni_del_mese(MaxGiorni, NuovoMese, Anno),
+       NuovoGiorno is Giorno - Sottraendo + MaxGiorni,
+       sottrai_giorni(0, data(NuovoGiorno, NuovoMese, Anno), DataArrivo)
     ).
 
 
 calcola_martedi_grasso(Anno,MartediGrasso) :-
-    % 1. Calcola la data di Pasqua per l'anno specificato
     pasqua(PGiorno, PMese, Anno),
-    
     sottrai_giorni(47, data(PGiorno, PMese, Anno), MartediGrasso).
     
 calcola_giovedi_grasso(Anno, GiovediGrasso) :-
