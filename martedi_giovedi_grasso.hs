f073fe06d7a5a6e1060d750763e4f420d306a98e
ImBelo <gianmarco.beligni@gmail.com>
2025-04-28 15:12:02 +0200

    colpa tua

--- martedi_giovedi_grasso.hs
+++ martedi_giovedi_grasso.hs
@@ -16,6 +16,14 @@ crea_data x Marzo anno    | x>0 && x<=31 = Calendario{giorno = x,mese = Marzo,an
 crea_data x Aprile anno   | x>0 && x<=30 = Calendario{giorno = x,mese = Aprile,anno = anno}
                           | otherwise = error $ show x ++ " il giorno deve essere tra 1-30"  
 
+giorniDelMese :: Mese -> Int -> Int
+giorniDelMese Febbraio anno
+  | controlla_bisestile anno = 29
+  | otherwise        = 28
+giorniDelMese mese _
+  | mese == Aprile = 30
+  | mese == Marzo = 31                     
+
 calcolo_pasqua :: Int -> Calendario
 calcolo_pasqua anno = crea_data giorno mese anno
   where giorno | z < 10 = z + 22
@@ -34,8 +42,20 @@ controlla_bisestile :: Int -> Bool
 controlla_bisestile anno = (anno `mod` 4 == 0 && anno `mod` 100 /= 0) || (anno `mod` 400 == 0)
 {-! DA FARE !-}
 calcola_martedi_giovedi_grasso :: Bool -> Calendario -> Calendario
-calcola_martedi_giovedi_grasso _ pasqua = pasqua
+calcola_martedi_giovedi_grasso turno_calcolo pasqua 
+        -- calcolo il martedi grasso o il giovedi grasso
+  | turno_calcolo == False = calcola_giorno_mese 47 pasqua
+  | otherwise = calcola_giorno_mese 52 pasqua
 
+calcola_giorno_mese :: Int -> Calendario -> Calendario
+calcola_giorno_mese giorni_scalare pasqua 
+  | (giorno pasqua) - giorni_scalare <= 0 = calcola_giorno_mese 0 calendario
+  | otherwise = Calendario{giorno = giorno pasqua, mese = mese pasqua,anno = anno pasqua}
+    where
+      calendario = Calendario{giorno = giorni_rimasti, mese = mese_precedente,anno = anno_corrente} 
+      giorni_rimasti = (giorno pasqua - giorni_scalare) + (giorniDelMese mese_precedente anno_corrente)
+      mese_precedente = pred (mese pasqua) 
+      anno_corrente = anno pasqua
 mostra_data :: Calendario -> String
 mostra_data giorno_calendario = unlines data_combinata
       where
@@ -179,11 +199,6 @@ cifreAscii =
 
 main :: IO ()
 main = do
-  putStrLn$ mostra_data(calcolo_pasqua 2003)
-  putStrLn$ mostra_data(calcolo_pasqua 2004)
-  putStrLn$ mostra_data(calcolo_pasqua 2005)
-  putStrLn$ mostra_data(calcolo_pasqua 2006)
-  putStrLn$ mostra_data(calcolo_pasqua 2007)
-  putStrLn$ mostra_data(calcolo_pasqua 2008)
-  putStrLn$ mostra_data(calcolo_pasqua 2009)
-  putStrLn$ mostra_data(calcolo_pasqua 2010)
+  putStrLn $ mostra_data $ calcolo_pasqua 2003
+  putStrLn ( mostra_data (calcola_martedi_giovedi_grasso False (calcolo_pasqua 2003)))
+  putStrLn ( mostra_data (calcola_martedi_giovedi_grasso True (calcolo_pasqua 2003)))
