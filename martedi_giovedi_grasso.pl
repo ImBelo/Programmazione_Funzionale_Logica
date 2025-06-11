ec86f8f4b1b518b688c63e54faa489ba3a46cb52
Elia <elia.renzoni03@gmail.com>
2025-06-11 10:58:22 +0200

    sistemato errori: aggiunto spazi tra predicati e cambiato commenti

--- martedi_giovedi_grasso.pl
+++ martedi_giovedi_grasso.pl
@@ -14,7 +14,23 @@ precedente(aprile, maggio).
 successivo(Mese1, Mese2):-
   precedente(Mese2, Mese1).
 
-/* regola che restituisce la data di Pasqua:
+
+% definizione del predicato principale
+programma :-
+  write('Programma per il calcolo di Giovedì e Martedì Grasso secondo il calendario Gregoriano\n'),
+  write('Inserire l''anno - tra il 1900 e il 2099 - per calcolare il Martedì Grasso\n'),
+  acquisisci_anno(PrimoAnno),
+  write('Inserire l''anno - tra il 1900 e il 2099 - per calcolare il Giovedì Grasso\n'),
+  acquisisci_anno(SecondoAnno),
+  calcola_martedi_grasso(PrimoAnno, MartediGrasso),
+  calcola_giovedi_grasso(SecondoAnno, GiovediGrasso),
+  data(Giorno, Mese, _) = MartediGrasso,
+  stampa_caratteri_giganti(Giorno, Mese),
+  data(GiornoG, MeseG, _) = GiovediGrasso,
+  stampa_caratteri_giganti(GiornoG, MeseG).  
+
+
+/* predicato che restituisce la data di Pasqua:
    gli argomenti sono il giorno, mese e anno */
 pasqua(Giorno, Mese, Anno) :-
     A is Anno mod 19,
@@ -41,7 +57,7 @@ pasqua(Giorno, Mese, Anno) :-
     ).
 
 
-/* regola per controllare che l'anno sia bisestile:
+/* predicato per controllare che l'anno sia bisestile:
    l'argomento è l'anno 
 */
 controlla_bisestile(Anno) :-
@@ -56,7 +72,8 @@ crea_data(Giorno, Mese, Anno) :-
     giorni_del_mese(GiornoMax, Mese, Anno),
     Giorno =< GiornoMax.
 
-/* regola che restituisce quanti giorno ci sono in un mese:
+
+/* predicato che restituisce quanti giorno ci sono in un mese:
    il primo argomento è il giorno
    il secondo argomento è il mese
    il terzo argomento è l'anno (importante per Febbraio) 
@@ -67,7 +84,7 @@ giorni_del_mese(31, Mese, _) :- Mese = marzo.
 giorni_del_mese(30, Mese, _) :- Mese = aprile.
 
 
-/* regola che calcola la sottrazione di giorni ad una data:
+/* predicato che calcola la sottrazione di giorni ad una data:
    il primo argomento è il numero di giorno da sottrarre
    il secondo argomento è la data a cui sottrarre i giorni 
    il terzo argomento è il risultato della sottrazione 
@@ -83,7 +100,8 @@ sottrai_giorni(Sottraendo, data(Giorno, Mese, Anno), DataArrivo) :-
        sottrai_giorni(0, data(NuovoGiorno, NuovoMese, Anno), DataArrivo)
     ).
 
-/* regola che calcola la data del Martedì o Giovedì grasso:
+
+/* predicato che calcola la data del Martedì o Giovedì grasso:
    il primo argomento è il giorno grasso
    il secondo argomento è la data di pasqua dell'anno in cui si vuole calcolare i giorni grassi 
 */
@@ -101,7 +119,8 @@ calcola_martedi_giovedi_grasso(martedi, Anno, MartediGrasso, _) :-
 calcola_martedi_giovedi_grasso(giovedi, Anno, _, GiovediGrasso) :-
     calcola_giovedi_grasso(Anno, GiovediGrasso).       
 
-/* regola che restituisce ASCII art di una cifra:
+
+/* predicato che restituisce ASCII art di una cifra:
    l'argomento è la cifra da cui prendere l'ASCII art 
 */
 giorno_ascii(Num, GiornoCodificato) :-
@@ -156,7 +175,8 @@ giorno_ascii(Num, GiornoCodificato) :-
                                      '    *',
                                      '*****']).
 
-/* regola che converte Mese nella sua rappresentazione ASCII art:
+
+/* predicato che converte Mese nella sua rappresentazione ASCII art:
    il primo argomento è il mese da cui prendere l'ASCII art 
    il secondo argomento è il mese codificato in ASCII art
 */
@@ -207,7 +227,8 @@ mese_ascii(Mese, MeseCodificato) :-
                                         '*  * ',
                                         '*   *']]).
 
-/* regola per acquisire l'anno dall'utente: 
+
+/* predicato per acquisire l'anno dall'utente: 
    l'argomento è l'anno che rientra nei limiti definiti dalla regola controlla_anno
    questa regola viene chiamata ricorsivamete fino a quando non è soddisfatta la regola controlla_anno
 */
@@ -225,7 +246,7 @@ acquisisci_anno(AnnoScelto) :-
   ).
 
     
-/* regola per controllare l'anno:
+/* predicato per controllare l'anno:
    gli utlimi due parametri indicano l'input inserito dall'utente
    e l'anno da restituire.
 */
@@ -236,28 +257,33 @@ controlla_anno(AnnoLetto, AnnoRestituire) :-
   ;   stampa_errore, 
       acquisisci_anno(AnnoRestituire)
   ).
-    
-/* regola per controllare l'anno
+
+
+/* predicato per controllare l'anno
    il primo argomento è l'anno da controllare,
    il secondo argomento è l'anno 
 */
 stampa_errore :-
   write('Input non valido. L''anno deve essere tra 1900 e 2099.\n').
    
-/* regola per stampare caratteri in formato gigante:
+
+/* predicato per stampare caratteri in formato gigante:
    il primo argomento è la lista dei caratteri della prima parola,
    il secondo argomento è la lista dei caratteri della seconda parola
    esempio: stampa_gigante("CIAO", "MONDO")
 */
 stampa_gigante(Caratteri1, Caratteri2) :-
   stampa_righe_giganti(0, Caratteri1, Caratteri2).
-/* regola ricorsiva per stampare righe di caratteri giganti:
+
+
+/* predicato ricorsivo per stampare righe di caratteri giganti:
    il primo argomento è l'indice della riga corrente (0-4),
    il secondo argomento è la lista dei caratteri della prima parola,
    il terzo argomento è la lista dei caratteri della seconda parola,
    caso base: quando raggiunge la 5a riga (indice 5) termina con cut (!)
 */
 stampa_righe_giganti(5, _, _) :- !.
+
 
 /* caso ricorsivo per stampare una riga di caratteri giganti:
    il primo argomento è l'indice della riga corrente,
@@ -276,10 +302,14 @@ stampa_righe_giganti(RigaIndice, Caratteri1, Caratteri2) :-
   nl,
   Successivo is RigaIndice + 1,
   stampa_righe_giganti(Successivo, Caratteri1, Caratteri2).
-/* regola per stampare una singola riga di caratteri:
+
+
+/* predicato per stampare una singola riga di caratteri:
    caso base: lista vuota, termina
 */
 stampa_riga(_, []).
+
+
 /* caso ricorsivo per stampare una riga di caratteri:
    il primo argomento è l'indice della riga da stampare,
    il secondo argomento è la lista dei caratteri,
@@ -293,7 +323,7 @@ stampa_riga(Index, [Lettera | Resto]) :-
   stampa_riga(Index, Resto).
 
 
-/* regola che trasforma una data nella rappesentazione ASCII art:
+/* predicato che trasforma una data nella rappesentazione ASCII art:
    l'argomento è una data 
 */
 stampa_caratteri_giganti(Giorno, Mese) :-
@@ -303,18 +333,3 @@ stampa_caratteri_giganti(Giorno, Mese) :-
   giorno_ascii(Decina, DecinaCodificata),
   mese_ascii(Mese, MeseCodificato),
   stampa_gigante([UnitaCodificata, DecinaCodificata], MeseCodificato).
-
-
-% definizione della regola principale
-programma :-
-  write('Programma per il calcolo di Giovedì e Martedì Grasso secondo il calendario Gregoriano\n'),
-  write('Inserire l''anno per calcolare il Martedì Grasso\n'),
-  acquisisci_anno(PrimoAnno),
-  write('Inserire l''anno per calcolare il Giovedì Grasso\n'),
-  acquisisci_anno(SecondoAnno),
-  calcola_martedi_grasso(PrimoAnno, MartediGrasso),
-  calcola_giovedi_grasso(SecondoAnno, GiovediGrasso),
-  data(Giorno, Mese, _) = MartediGrasso,
-  stampa_caratteri_giganti(Giorno, Mese),
-  data(GiornoG, MeseG, _) = GiovediGrasso,
-  stampa_caratteri_giganti(GiornoG, MeseG).
